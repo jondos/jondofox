fd5bbd7903a6865b1f99c8ba0c00c34f6df9ef33
EKnobloch <ezra.knobloch@jondos.de>
2016-10-20 18:48:53 +0200

    #1326 feature added?
    maybe this is the solution, maybe not..

--- observer.js
+++ observer.js
@@ -245,6 +245,17 @@ var httpRequestObserver = {
               }
           
           }
+          
+          try{
+          
+            var referrer = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService).newURI("https://nothing.org", "", null);
+          
+            httpChannel.setReferrerWithPolicy(referrer, httpChannel.REFERRER_POLICY_NO_REFERRER);
+          
+          }
+          catch(e){
+          
+          }
       
       }
 
